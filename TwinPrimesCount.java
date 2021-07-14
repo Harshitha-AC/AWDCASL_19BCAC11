2fd22be01736381a364b504751f2fbd32d941855
Harshitha-AC <86794732+Harshitha-AC@users.noreply.github.com>
2021-07-14 22:43:36 +0530

    Add files via upload

---
+++ TwinPrimesCount.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class TwinPrimesCount
+{
+ static void printPrime(int a,int b)
+{
+int n=b-a+1;int m=0;
+int i=0,count=0;
+int ch[]=new int[n];
+while(a<=b)
+{
+for(int j=1;j<=a;j++)
+{
+if(a%j==0)
+count++;
+}
+if(count==2)
+{
+if(i<n)
+{
+ch[i]=a;
+i++;
+}
+}
+a++;
+count=0;
+}
+for(int k=0;k<n;k++)
+{
+if(k+1<n)
+{
+if(ch[k+1]-ch[k]==2)
+{
+m++;
+System.out.println("twin primes are:"+ch[k]+"\t"+ch[k+1]);
+}
+}
+}
+
+System.out.println("number of twin pairs:"+m);
+}
+public static void main(String args[])
+{
+System.out.println("enter range");
+Scanner sc=new Scanner(System.in);
+int r1,r2;
+r1=sc.nextInt();
+r2=sc.nextInt();
+printPrime(r1,r2);
+}
+}
